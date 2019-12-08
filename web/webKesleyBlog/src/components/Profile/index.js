d1992266090d7429243fac100d4c8bd8c80a5947
Kesley David <kesleygti@gmail.com>
2019-12-08 19:04:07 -0300

    [web] [dev] [new] Created Components Test

---
+++ web/webKesleyBlog/src/components/Profile/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+// import { Container } from './styles';
+
+const Profile = () => (
+  <div className="Profile-wrapper">
+    <h1>Kesley David</h1>
+    <h2>Development Manager</h2>
+    <p>lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem </p>
+  </div>
+);
+
+export default Profile;
