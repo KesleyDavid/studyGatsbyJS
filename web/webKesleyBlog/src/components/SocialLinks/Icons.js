d5efeb68bd461af9085ef9e4e0b707925675d283
Kesley David <kesleygti@gmail.com>
2019-12-10 09:07:51 -0300

    [web] [dev] [new] Social Links

---
+++ web/webKesleyBlog/src/components/SocialLinks/Icons.js
@@ -0,0 +1,25 @@
+import { Linkedin } from "styled-icons/boxicons-logos/Linkedin"
+import { Github } from "styled-icons/boxicons-logos/Github"
+import { Twitter } from "styled-icons/boxicons-logos/Twitter"
+import { Telegram } from "styled-icons/boxicons-logos/Telegram"
+import { Discord } from "styled-icons/boxicons-logos/Discord"
+import { Mail } from "styled-icons/material/Mail"
+// import { Youtube } from "styled-icons/boxicons-logos/Youtube"
+// import { Instagram } from "styled-icons/boxicons-logos/Instagram"
+// import { Behance } from "styled-icons/boxicons-logos/Behance"
+// import { Gitlab } from "styled-icons/boxicons-logos/Gitlab"
+
+const Icons = {
+  Linkedin,
+  Github,
+  Twitter,
+  Telegram,
+  Discord,
+  Mail,
+// Youtube,
+// Instagram,
+// Behance,
+// Gitlab,
+}
+
+export default Icons
---
+++ web/webKesleyBlog/src/components/SocialLinks/content.js
@@ -0,0 +1,46 @@
+const handle = 'KesleyDavid';
+const handleTwitter = 'kesley_david';
+// const handleYoutube = 'UCtwG53vrewBT4L_nVnCor5w';
+const handleDiscord = 'vnwMJxp';
+
+const links = [
+  {
+    label: "Linkedin",
+    url: `https://www.linkedin.com/in/${handle}`,
+  },
+  {
+    label: "Github",
+    url: `https://github.com/${handle}`,
+  },
+  {
+    label: "Twitter",
+    url: `https://twitter.com/${handleTwitter}`,
+  },
+  {
+    label: "Telegram",
+    url: `https://t.me/${handle}`,
+  },
+  {
+    label: "Discord",
+    url: `https://discord.gg/${handleDiscord}`,
+  },
+  {
+   label: "Mail",
+   url: `mailto:kesleygti@email.com`,
+  },
+  // {
+  //   label: "Youtube",
+  //   url: `https://www.youtube.com/channel/${handleYoutube}`,
+  // },
+  // {
+  //   label: "Behance",
+  //   url: `http://be.net/${handle}`,
+  // },
+  // {
+  //   label: "Gitlab",
+  //   url: `https://gitlab.com/${handle}`,
+  // },
+  // {
+]
+
+export default links
