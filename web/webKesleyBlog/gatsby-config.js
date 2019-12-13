24b6b60c4fc18b5b03ebb1abafa881ef645e4934
Kesley David <kesleygti@gmail.com>
2019-12-13 18:55:33 -0300

    [web] [dev] [update] [bug] Color Manifest

--- web/webKesleyBlog/gatsby-config.js
+++ web/webKesleyBlog/gatsby-config.js
@@ -87,7 +87,7 @@ module.exports = {
         short_name: `Kesley DEV`,
         start_url: `/`,
         background_color: `#16202c`,
-        theme_color: `#6616202c399`,
+        theme_color: `#16202c`,
         display: `minimal-ui`,
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
